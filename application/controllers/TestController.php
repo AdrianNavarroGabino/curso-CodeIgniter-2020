648fa0dd9720697b59fe0817f8bbe0b69990b6a0
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2020-06-22 18:02:47 +0200

    02 - Creando el primer controlador

---
+++ application/controllers/TestController.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class TestController extends CI_Controller {
+
+	public function prueba()
+	{
+		//$this->load->view('');
+		echo "hola";
+	}
+}
